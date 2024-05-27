f15df40b8c05b90b67babd5d1a92403ec48e7c34
German Alvarez Manzini <71088676+germanalvarez777@users.noreply.github.com>
2024-05-27 01:03:50 -0300

    Add files via upload

---
+++ cliente_e.py
@@ -0,0 +1,23 @@
+import Pyro5.api
+import unicodedata
+
+def correr_cliente():
+    server_ip = "192.168.1.44"
+
+    # Se crea un proxy para el objeto remoto registrado como "ejercicio1_rpc" en la IP y puerto especificados.
+    Materias = Pyro5.api.Proxy(f"PYRO:ejercicio4_rpc@{server_ip}:57175")
+    dia = input("Ingrese un dia para averiguar sus materias: ").lower()
+    
+     # Descompone el texto quitando los caracteres con acentos
+    texto_normalizado = unicodedata.normalize('NFKD', dia)
+    # Filtra solo los caracteres base (sin acentos)
+    dia = ''.join([c for c in texto_normalizado if not unicodedata.combining(c)])
+
+    try:
+        resultado = Materias.get_materias(dia)
+        print(resultado)
+    except ValueError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    correr_cliente()
---
+++ servidor_e.py
@@ -0,0 +1,34 @@
+import Pyro5.api
+import socket
+
+@Pyro5.api.expose
+class Materias(object):
+    __materias = {
+        "lunes": ["Base de Datos", "Ingenieria de Software II"],
+        "martes": ["Ingenieria de Software II"],
+        "miercoles": ["Base de Datos", "Legislacion Profesional"],
+        "jueves": ["Paradigmas de Lenguaje"],
+        "viernes": ["Paradigmas de Lenguaje","Base de Datos"]
+    }
+    def get_materias (self, dia):
+        while dia == 'sabado' or dia == 'domingo':
+            if dia not in self.__materias:
+                raise ValueError(f"No hay materias para el día {dia}.")
+        else:
+            return self.__materias[dia]
+
+    def get_diccionario(self):
+        return self.__materias
+    
+def correr_servicio():
+    hostname = socket.gethostname()
+    direccion_ip = socket.gethostbyname(hostname)
+    daemon = Pyro5.api.Daemon(host="0.0.0.0")
+    uri = daemon.register(Materias, "ejercicio4_rpc")
+    print(f"Servidor listo. URI del objeto: {uri}")
+    print(f"Dirección IP del servidor: {direccion_ip}")
+    daemon.requestLoop()  
+
+
+if __name__ == "__main__":
+    correr_servicio()
