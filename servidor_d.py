e94a92b46dfb0a7710eb99a80b0a4c818b51736c
German Alvarez Manzini <71088676+germanalvarez777@users.noreply.github.com>
2024-05-27 03:42:43 -0300

    Add files via upload

---
+++ servidor_d.py
@@ -0,0 +1,19 @@
+import Pyro5.api
+import socket
+
+@Pyro5.api.expose
+class ProductoVector:
+    def producto_escalar(self, vector, escalar):
+        return [v * escalar for v in vector]
+
+def correr_servicio():
+    hostname = socket.gethostname()
+    direccion_ip = socket.gethostbyname(hostname)
+    daemon = Pyro5.api.Daemon(host="0.0.0.0")
+    uri = daemon.register(ProductoVector, "ejercicio3_rpc")
+    print(f"Servidor listo. URI del objeto: {uri}")
+    print(f"Dirección IP del servidor: {direccion_ip}")
+    daemon.requestLoop()  
+
+if __name__ == "__main__":
+    correr_servicio()
